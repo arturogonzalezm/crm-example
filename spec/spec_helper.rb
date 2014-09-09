40e1c26326195df12cf282dfadf496d86b032be7
Arturo Gonzalez <arturo@arturosolutions.ch>
2014-09-09 22:31:27 +1000

    PostgreSQL added

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@
 #------------------------------------------------------------------------------
 require 'rubygems'
 
-ENV["RAILS_ENV"] = 'test'
+ENV["RAILS_ENV"] = 'development'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'capybara/rails'
